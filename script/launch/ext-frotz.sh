ada38917a9779737ad263146269c3f3cf47be946
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2026-04-25 09:13:06 +1000

    Modified terminal launch scripts to use custom config path of name

--- script/launch/ext-frotz.sh
+++ script/launch/ext-frotz.sh
@@ -19,5 +19,8 @@ SETUP_SDL_ENVIRONMENT
 
 SET_VAR "system" "foreground_process" "muterm"
 
-/opt/muos/frontend/muterm --gl -- /opt/muos/bin/dfrotz "$FILE"
+TERM_CFG_DIR="/opt/muos/share/info/config/Terminal"
+mkdir -p "$TERM_CFG_DIR"
+
+/opt/muos/frontend/muterm --gl -c "$TERM_CFG_DIR"/"$NAME".conf -- /opt/muos/bin/dfrotz "$FILE"
 
